8e4136ededec1fd43b0f016634f4eb0c5c6aa3fd
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2019-12-02 13:16:15 -1000

    Small change to PersonCard styling

--- starwars/src/components/PersonCard.js
+++ starwars/src/components/PersonCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const CharacterCard = styled.div`
-  background: gray;
+  background: lightgray;
   color: black;
   width: 250px;
   max-height: 300px;
